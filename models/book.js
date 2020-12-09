c4584854c8131f1a9e4b5a5585194e0e5dd5e5a2
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-12-08 22:12:45 -0600

    added id and routes

--- models/book.js
+++ models/book.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+    id: {type: String, required: true},
     title: { type: String, required: true},
     authors: { type: Array, required: true},
     description: { type: String, required: true},
--- routes/api/books.js
+++ routes/api/books.js
@@ -4,8 +4,12 @@ const router = require("express").Router()
 router.route("/").get(bookController.getBooks)
                  .post(bookController.saveBooks)
 
- 
+
+// in order to add/delete books to the database, match with :id
 router.route("/:id").delete(bookController.deleteBooks)
+                .get(bookController.getBooks)
+                .delete(bookController.deleteBooks)
+
 
 
 module.exports = router;
